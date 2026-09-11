d8f1a45a747831aa7333a0138f258e36cd0e3fb6
Dev Karan Singh <devkaran.official.88@gmail.com>
2026-09-11 21:31:55 +0530

    Fix Two Sum solution structure

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,7 +1,4 @@
-package Arrays;
-
-public class TwoSum {
-    class Solution {
+ class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0 ; i < nums.length ; i++)
         {
@@ -17,5 +14,6 @@ public class TwoSum {
         
     }
 }
+
     
-}
+
